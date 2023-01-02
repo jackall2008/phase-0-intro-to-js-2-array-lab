269d405d351d1a81e4fcd7f027cb4bc460cbf921
Julian Patterson <julian.patterson@gmail.com>
2023-01-01 22:43:20 -1000

    commit tree 6154ffcc7e6169995d0cdf27f00d2d56bd8fd26b

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    const newNewCats = [name, ...cats];
+    return newNewCats;
+}
+
+function removeLastCat() {
+    const removedCats = cats.slice(0, -1);
+    return removedCats;
+}
+
+function removeFirstCat() {
+    const moreRemovedCats = cats.slice(1);
+    return moreRemovedCats;
+}
